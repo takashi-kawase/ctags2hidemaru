634cb1017bb9c959301c1a51b4fadec812630c0f
Takashi KAWASE <takashi_kawase@nifty.com>
2013-08-11 21:32:33 +0900

    初版作成
    Ver1.0.1.0相当のコード
    ただし、AssemblyVersionAttributeを変更していることに注意

---
+++ ctags2hm/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("ctags2hm")]
+[assembly: AssemblyDescription("ctags to Hidemaru converter")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Takashi KAWASE")]
+[assembly: AssemblyProduct("ctags2hm")]
+[assembly: AssemblyCopyright("Copyright © 2013 Takashi KAWASE")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
+// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
+[assembly: Guid("5687a83f-85ac-42c5-a9f2-2f35ac5d353d")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersionAttribute("1.0.*")]
---
+++ ctags2hm/ctags2hm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace ctags2hm
+{
+    class ctags2hm
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: ctags2hm.exe tags_file");
+                return;
+            }
+
+            string inFileName = args[0];
+
+            if (!File.Exists(inFileName))
+            {
+                Console.WriteLine("Input file not found.");
+                return;
+            }
+
+            string[] inStr = File.ReadAllLines(inFileName);
+
+            string dir = Path.GetDirectoryName(inFileName);
+            dir = string.Concat(dir, "\\");
+
+            ArrayList outputStringArray = new ArrayList();
+            foreach(string str in inStr)
+            {
+                if (! str.StartsWith("!_TAG_"))
+                {
+                    string[] stringSeparators = { "\t" };
+                    string[] strParts = str.Split(stringSeparators, 3, StringSplitOptions.RemoveEmptyEntries);
+                    
+                    string name = strParts[0];
+                    string file = strParts[1];
+                    string line = strParts[2];
+
+                    int i = line.IndexOf(";\"");
+                    if (i != -1)
+                    {
+                        line = line.Remove(i);
+                    }
+
+                    if (file.StartsWith(dir, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        file = file.Remove(0, dir.Length);
+                    }
+
+                    string outStrTemp = file + "(" + line + ")" + " : " + name;
+
+                    outputStringArray.Add(outStrTemp);
+                }
+            }
+            string[] outStr = (string[])outputStringArray.ToArray(typeof(string));
+
+            File.WriteAllLines(args[0], outStr);
+        }
+    }
+}
